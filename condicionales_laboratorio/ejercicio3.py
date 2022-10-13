46bae348de00385afa3333460be9f23441e0c476
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-13 12:09:53 -0300

    Create ejercicio3.py

---
+++ condicionales_laboratorio/ejercicio3.py
@@ -0,0 +1,20 @@
+###
+Ejercicio 3
+Dadas 2 variables: a = 20 y b = 10, mostrar por
+pantalla su suma, resta, multiplicación y división.
+###
+
+a = 20
+b = 10
+
+suma = a + b
+print("La suma es", suma)
+
+resta = a - b
+print("La resta es", resta)
+
+multiplicacion = a * b
+print("La multiplicacion es", multiplicacion)
+
+division = a / b
+print("La division es" , division)
