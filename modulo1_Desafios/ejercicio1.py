20e20a3ad3b0d30a07925cce5996d600739055a0
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-13 12:18:17 -0300

    Create ejercicio1.py

---
+++ modulo1_Desafios/ejercicio1.py
@@ -0,0 +1,12 @@
+###
+Tomás rindió 3 exámenes, se desea saber su
+promedio y mostrarlo por pantall
+###
+
+nota_uno = 10
+nota_dos = 6
+nota_tres = 8
+cantidad_examenes = 3
+
+promedio = (nota_uno + nota_dos + nota_tres) / cantidad_examenes
+print("El promedio de Tomas es ", promedio)
