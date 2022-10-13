3b3a78cf3a74eb2bc4af220d200d8771a3d3a754
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-13 12:02:29 -0300

    Create ejercicio1.py

---
+++ condicionales_laboratorio/ejercicio1.py
@@ -0,0 +1,18 @@
+###
+
+Armar una frase con las 3 variables dadas y
+mostrarla por pantalla.
+
+Es obligatorio usar las 3 variables, pero también
+podés agregar palabras vos para generar una
+frase. No importa el orden que elijas para las
+variables.
+
+###
+
+
+texto_uno = "potente"
+texto_dos = "sol"
+texto_tres = "triunfo"
+oracion = ("El " + texto_uno +" " + texto_dos + " ilumina" + " el" + texto_tres )
+print (oracion)
