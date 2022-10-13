3c63c2115081374442ccd19be3b31c9c0f32706b
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-13 12:25:01 -0300

    Create ejercicio2.py

---
+++ modulo1_Desafios/ejercicio2.py
@@ -0,0 +1,13 @@
+
+###
+Hacer un programa que determine si una persona es menor de edad o mayor de edad,
+teniendo la edad en una variable.
+Probar el código cambiando el valor de la variable “edad”.
+###
+
+edad = 21
+
+if edad >= 18:
+  print("Es mayor de edad")
+else:
+  print("Es menor de edad")
