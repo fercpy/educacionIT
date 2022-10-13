d8ca134a3701f5f73830d0e2a0b5a08f36e19444
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-13 12:04:03 -0300

    Create ejercicio2.py

---
+++ condicionales_laboratorio/ejercicio2.py
@@ -0,0 +1,11 @@
+###
+Ejercicio 2
+Realizar un programa que tenga 2 variables,
+base = 10 y altura = 5, calcular el área de
+un rectángulo y mostrar por pantalla.
+###
+ 
+base = 10
+altura = 5
+area_rectangulo = base * altura
+print("El area del rectangulo es" , area_rectangulo)
