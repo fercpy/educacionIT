d9b593eb1e0f33530700179c3605996fce0ae2c5
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-21 11:08:34 -0300

    Create ejercicio1.py

---
+++ modulo2/laboratorioAdicional/ejercicio1.py
@@ -0,0 +1,14 @@
+###
+
+Crea un programa que permita ingresar 2 cadenas por consola y luego
+las compare.
+Imprimiendo un mensaje en que caso qeu sean iguales y otro en caso que sean diferentes
+
+###
+
+cadena_a = input("Ingrese una cadena: ")
+cadena_b = input("Ingrese otra cadena: ")
+if a == b:
+    print("Las cadenas son iguales.")
+else:
+    print("Las cadenas son diferentes.")
