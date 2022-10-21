2b5f9577774d5f6605d3d97eb2ce5dbc766c155c
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-21 11:09:56 -0300

    Update ejercicio1.py

--- modulo2/laboratorioAdicional/ejercicio1.py
+++ modulo2/laboratorioAdicional/ejercicio1.py
@@ -7,8 +7,8 @@ Imprimiendo un mensaje en que caso qeu sean iguales y otro en caso que sean dife
 ###
 
 cadena_a = input("Ingrese una cadena: ")
-cadena_b = input("Ingrese otra cadena: ")
-if a == b:
+cadena_b = input("Ingrese una cadena: ")
+if cadena_a == cadena_b:
     print("Las cadenas son iguales.")
 else:
     print("Las cadenas son diferentes.")
