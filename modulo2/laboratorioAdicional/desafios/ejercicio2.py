61155c2784d559c8bf0a2e847fe78dc5afa84810
fercpy <115718367+fercpy@users.noreply.github.com>
2022-10-25 13:24:50 -0300

    Create ejercicio2.py

---
+++ modulo2/laboratorioAdicional/desafios/ejercicio2.py
@@ -0,0 +1,6 @@
+year = int(input("Ingrese un año: "))
+
+if( ((year % 4 == 0) and (year % 100 != 0)) or (year % 400==0) ):
+    print("El año es bisiesto")
+else:
+    print("El año no es bisiesto")
